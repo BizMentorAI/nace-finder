9dfe05642149e574d3e7e83d365de00c7bedfc08
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2023-05-11 15:50:02 -0600

    Will fx?

--- src/bm/nace_finder/layout.cljs
+++ src/bm/nace_finder/layout.cljs
@@ -24,9 +24,7 @@
                    ;; Fix page height for mobile Safari.
                    ;; Doing this in CSS doesn't work as Chrome matches it also.
                    ;; https://allthingssmitty.com/2020/05/11/css-fix-for-100vh-in-mobile-webkit
-                   (set! (. this -style) -minHeight "-webkit-fill-available")
-                   ;(js* "this.style.minHeight = '-webkit-fill-available'")
-                   )))
+                   (js* "this.style.minHeight = '-webkit-fill-available'"))))
 
   Object
   (isMobileSafari [this]
