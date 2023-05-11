811165cc30ef21eb54f283d27da24a5593c531fc
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2023-05-11 16:12:26 -0600

    X

--- src/bm/nace_finder/layout.cljs
+++ src/bm/nace_finder/layout.cljs
@@ -18,11 +18,14 @@
                  (append-child this (tag :main (tag :slot)))
                  (append-child this (tag :bm-footer))
 
-                 (set! dbg -innerHTML (str js/screen.width "x" js/screen.height))
+                 (.screenDebugInfo this dbg)
+                 (.addEventListener js/window
+                                    "resize"
+                                    (fn [event]
+                                      (js/console.log "Window resized.")
+                                      (.screenDebugInfo this dbg)))
 
                  (when (.isMobileSafari this)
-                   (js/console.log "Mobile Safari!")
-                  (set! dbg -innerHTML (str "(i) " js/screen.width "x" js/screen.height))
                    ;; Fix page height for mobile Safari.
                    ;; Doing this in CSS doesn't work as Chrome matches it also.
                    ;; https://allthingssmitty.com/2020/05/11/css-fix-for-100vh-in-mobile-webkit
@@ -33,6 +36,39 @@
                   (and
                    (js/window.navigator.userAgent.match #"(?i)iphone|ipad")
                    (js/window.navigator.userAgent.match #"(?i)WebKit")
-                   (not (js/window.navigator.userAgent.match #"(?i)CriOS")))))
+                   (not (js/window.navigator.userAgent.match #"(?i)CriOS"))))
+
+  (screenDebugInfo [this div]
+                   (.isMobileSafari this)
+                   (set! div -innerHTML
+                         (str (when (.isMobileSafari this) "(i) ")
+                              (cond
+                                (js/window.matchMedia "(max-width: 600px)") "XS "
+                                (js/window.matchMedia "(max-width: 900px)") "SM "
+                                (js/window.matchMedia "(max-width: 1200px)") "LG ")
+                              js/window.innerWidth "x" js/window.innerHeight
+                              " (" js/window.devicePixelRatio ")"))))
 
 (register-custom-element :bm-layout Layout)
+
+;; /* XS retina */
+;; @media (-webkit-min-device-pixel-ratio: 2) (min-resolution: 192dpi) and (max-width: 1000px) {
+;;   body { background: "red"; }
+;;   #screen::after { content: "XS(r)"; }
+;; }
+
+;; window.matchMedia("(max-width: 700px)")
+;; /* XS */
+;; @media (max-width: 650px) {
+;;   #screen::after { content: "XS"; }
+;; }
+
+;; /* SM */
+;; @media (max-width: 900px) {
+;;   #screen::after { content: "SM"; }
+;; }
+
+;; /* LG */
+;; @media (min-width: 1200px) {
+;;   #screen::after { content: "LG"; }
+;; }
