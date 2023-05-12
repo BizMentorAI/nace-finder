fe41355cdf8ebdd56137b32358fc477dd80170d7
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2023-05-11 18:33:04 -0600

    WIP

--- src/bm/components/autocomplete.cljs
+++ src/bm/components/autocomplete.cljs
@@ -1,8 +1,12 @@
 (ns bm.components.autocomplete
   (:require [shadow.cljs.modern :refer (defclass)]
             [shadow.resource :refer (inline)]
+            [clojure.edn :as edn]
             [bm.web-components.utils :refer
              (BMElement tag append-child event-listener register-custom-element)]))
+
+(def data (edn/read-string (inline "./autocomplete.edn")))
+(js/console.log data)
 
 (defclass Autocomplete
   (extends BMElement)
@@ -10,7 +14,8 @@
 
   (constructor [this]
                (super)
-               (let [autocomplete (tag :input {:placeholder "🔎  Type in a product or service keyword."})]
+               (let [placeholder "🔎  Type in a product or service keyword."
+                     autocomplete (tag :input {:placeholder placeholder})]
                  (append-child this (tag :style (inline "./autocomplete.css")))
                  (append-child this autocomplete)
                  (set! input autocomplete)
@@ -19,14 +24,24 @@
   Object
   (handleInput [this]
                (fn [event]
-                 (js/console.log :handleInput {:value (.value this)})
-                 (js/console.log event)
-                 ; TODO: filter options.
-                 ; We do not need event.target by the way, we can just
-                 ; refer the input field directly.
-                 ))
+                 (.buildMenu this (.filterItems this data (.value this)))))
+
+  (filterItems [this data search-term]
+               (filter identity
+                       (map (fn [l4-item]
+                              (.matchL4Item this l4-item))
+                            data)))
+
+  ; Either return nil or
+  ; {"L4 title": [L6 item I, L6 item II...]}
+  (matchL4Item [this item]
+               true)
+
+  (buildMenu [this items]
+             (js/console.log :menu items :count (count items)))
 
   (value [this] (.-value input))
+
   (setValue [this value] (set! input -value value)))
 
 (register-custom-element :bm-autocomplete Autocomplete)
